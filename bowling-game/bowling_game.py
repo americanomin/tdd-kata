731f9f8c328965c986037d9d379a529fea402209
lee.jimin <lee.jimin@29cm.co.kr>
2018-12-09 03:34:09 +0900

    [kata-2차] Bowling Game Kata 3차 테스트코드 추가
    Test Description
    - test_score_1번_스페어처리를_하고_3개의_핀을_쓰러트린_경우_스코어는_16점이다

--- bowling-game/bowling_game.py
+++ bowling-game/bowling_game.py
@@ -1,10 +1,21 @@
 class BowlingGame:
+    TOTAL_FRAME_COUNT = 10
+
     def __init__(self):
-        self.rolls = []
+        self.__rolls = []
 
     def roll(self, pins):
-        self.rolls.append(pins)
+        self.__rolls.append(pins)
 
     @property
     def score(self):
-        return sum(self.rolls)
+        score = 0
+        roll_index = 0
+        for frame_index in range(0, self.TOTAL_FRAME_COUNT):
+            frame_score = self.__rolls[roll_index] + self.__rolls[roll_index + 1]
+            roll_index = frame_index + 2
+            if frame_score == 10:
+                frame_score += self.__rolls[roll_index]
+
+            score += frame_score
+        return score
--- bowling-game/test_bowling_game.py
+++ bowling-game/test_bowling_game.py
@@ -5,10 +5,6 @@ class TestBowlingGame:
     def setup_method(self):
         self.bowling_game = BowlingGame()
 
-    def roll_many(self, pins, times):
-        for i in range(times):
-            self.bowling_game.roll(pins)
-
     def test_score_핀을_1개도_쓰러트리지_못한_경우_스코어는_0점이다(self):
         self.roll_many(0, 20)
         assert self.bowling_game.score == 0
@@ -16,3 +12,17 @@ class TestBowlingGame:
     def test_score_핀을_20번_연속으로_1개씩_쓰러트린_경우_스코어는_20점이다(self):
         self.roll_many(1, 20)
         assert self.bowling_game.score == 20
+
+    def test_score_1번_스페어처리를_하고_3개의_핀을_쓰러트린_경우_스코어는_16점이다(self):
+        self.roll_spare()
+        self.bowling_game.roll(3)
+        self.roll_many(0, 17)
+        assert self.bowling_game.score == 16
+
+    def roll_many(self, pins, times):
+        for i in range(times):
+            self.bowling_game.roll(pins)
+
+    def roll_spare(self):
+        self.bowling_game.roll(5)
+        self.bowling_game.roll(5)
